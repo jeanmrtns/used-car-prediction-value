d42c7743f28bee7395814d375829126ba512fe29
Jean Martins <jeanmrtns4@gmail.com>
2023-03-05 22:50:09 -0300

    feat: users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+interface CreateUserRequest {
+  email: string;
+  password: string;
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async create({ email, password }: CreateUserRequest) {
+    const userExists = await this.usersRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const user = this.usersRepository.create({
+      email,
+      password,
+    });
+
+    return this.usersRepository.save(user);
+  }
+
+  async findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async find(email: string) {
+    return this.usersRepository.find({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async update(id: number, userData: Partial<User>) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User does not exists');
+    }
+
+    Object.assign(user, userData);
+
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('User does not exists');
+    }
+
+    return this.usersRepository.remove(user);
+  }
+}
