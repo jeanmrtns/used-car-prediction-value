6b16af5d568bb43d910f7d89500438a2580433a2
Jean Martins <jeanmrtns4@gmail.com>
2023-03-05 22:50:58 -0300

    feat: create entities

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
 
   @Column()
   price: number;
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,38 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import {
+  AfterInsert,
+  AfterUpdate,
+  BeforeRemove,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    unique: true,
+  })
   email: string;
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Added user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User with id ${this.id} updated`);
+  }
+
+  @BeforeRemove()
+  logRemove() {
+    console.log(`User with id ${this.id} removed`);
+  }
 }
