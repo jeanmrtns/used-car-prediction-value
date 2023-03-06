051e2a65f4b906b59d6d3526aad2abbe9b4e0c42
Jean Martins <jeanmrtns4@gmail.com>
2023-03-05 22:51:26 -0300

    feat: create reports service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportsRepository: Repository<Report>,
+  ) {}
+
+  async create(price: number) {
+    const report = this.reportsRepository.create({
+      price,
+    });
+
+    return this.reportsRepository.save(report);
+  }
+
+  async find() {
+    return this.reportsRepository.find();
+  }
+
+  async findById(id: number) {
+    return this.reportsRepository.findBy({
+      id,
+    });
+  }
+}
